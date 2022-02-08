4565f9c36d5570ad41010326e4f4e0dd563c0f50
flaviobag <flaviobag@gmail.com>
2022-02-08 09:58:07 -0300

    year2

---
+++ scripts/year2.js
@@ -0,0 +1,11 @@
+
+function year2(itens) {
+var itens = new Array("maça", "pera", "abacate", "uva")
+
+for(i = 0; i < itens.length; i++){
+   console.log(itens[i])
+};
+return(itens[i]);
+}
+
+console.log(year2(5))
