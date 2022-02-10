ed1728b44e4b93d2dfdf3eb4415c849ee0e4ff37
flaviobag <flaviobag@gmail.com>
2022-02-10 08:19:57 -0300

    year4

---
+++ scripts/year4.js
@@ -0,0 +1,13 @@
+function year4(num) {
+
+    const numero1 = 5;
+    const numero2 = 10;
+
+    const control1 = numero2;
+    
+
+    console.log(numero2, numero1)
+    return(num);
+}
+
+console.log(year4())
