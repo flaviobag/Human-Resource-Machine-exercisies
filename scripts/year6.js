33a4568883e5c390f395c0b50d7825f4e064dd9d
flaviobag <flaviobag@gmail.com>
2022-02-11 08:33:09 -0300

    year6

---
+++ scripts/year6.js
@@ -0,0 +1,13 @@
+function year6(num) {
+
+    const numero1 = 5;
+    const numero2 = 10;
+
+    const sum = numero1 + numero2;
+    
+
+    console.log(sum)
+    return(num);
+}
+
+console.log(year6())
